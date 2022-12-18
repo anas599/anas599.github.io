f9943b74a263f9e4b5fd73396fe6a574b7bafbbb
anas599 <anas599@yahoo.com>
2022-12-18 23:05:55 +0300

    Added nav menu close icon

--- jsfile.js
+++ jsfile.js
@@ -1,8 +1,16 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
-  if (x.style.display === "flex") {
+  var closeIcon = document.querySelector(".close-icon");
+  var barsIcon = document.querySelector(".icon");
+  if (x.classList.contains("open")) {
+    x.classList.remove("open");
     x.style.display = "none";
+    closeIcon.style.display = "none";
+    barsIcon.style.display = "block";
   } else {
+    x.classList.add("open");
     x.style.display = "flex";
+    closeIcon.style.display = "block";
+    barsIcon.style.display = "none";
   }
 }
