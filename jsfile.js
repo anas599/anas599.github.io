1da0c4b32bf602caec1a052815db6c78864b3cd3
anas599 <anas599@yahoo.com>
2022-12-20 00:20:00 +0300

    Reomved logo when menu is opend

--- jsfile.js
+++ jsfile.js
@@ -1,3 +1,4 @@
+const logo = document.querySelector('.active');
 const icon = document.querySelector('.icon i');
 const x = document.getElementById('myLinks');
 const mobileMenuOptions = document.querySelectorAll('.mobile-menu-option');
@@ -7,9 +8,12 @@ function myFunction() {
   if (icon.classList.contains('fa-bars')) {
     icon.classList.replace('fa-bars', 'fa-times');
     x.style.display = 'flex';
+    logo.style.display = 'none';
   } else {
     icon.classList.replace('fa-times', 'fa-bars');
     x.style.display = 'none';
+    logo.style.display = "flex";
+
   }
 
   mobileMenuOptions.forEach((option) => {
