9d17c33597cc1bb9c6c8c716ad49cfba1db3f369
anas599 <anas599@yahoo.com>
2022-12-19 21:15:23 +0300

    Fix JS file eslint errors

--- jsfile.js
+++ jsfile.js
@@ -1,17 +1,14 @@
-let icon = document.querySelector(".icon i");
-let x = document.getElementById("myLinks");
+const icon = document.querySelector('.icon i');
+const x = document.getElementById('myLinks');
 
 function myFunction() {
-  if (icon.classList.contains("fa-bars")) {
-    icon.classList.remove("fa-bars");
-    icon.classList.add("fa-times");
-    x.style.display = "flex";
-
+  if (icon.classList.contains('fa-bars')) {
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-times');
+    x.style.display = 'flex';
   } else {
-    
-    icon.classList.remove("fa-times");
-    icon.classList.add("fa-bars");
-    x.style.display = "none";
-
+    icon.classList.remove('fa-times');
+    icon.classList.add('fa-bars');
+    x.style.display = 'none';
   }
 }
