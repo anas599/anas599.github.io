93ee38fdccb085f8b67a24a4a578dbe810bb9a99
AstroboyReloaded <astroboy.reloaded@gmail.com>
2022-12-19 13:43:30 -0600

    Switch-replace-args

--- jsfile.js
+++ jsfile.js
@@ -5,7 +5,7 @@ const mobileMenuOptions = document.querySelectorAll('.mobile-menu-option');
 // eslint-disable-next-line no-unused-vars
 function myFunction() {
   if (icon.classList.contains('fa-bars')) {
-    icon.classList.replace('fa-times', 'fa-bars');
+    icon.classList.replace('fa-bars', 'fa-times');
     x.style.display = 'flex';
   } else {
     icon.classList.replace('fa-times', 'fa-bars');
