be97607bbca4ed7edc82ef75f4be4bcf4ea3214c
anas599 <anas599@yahoo.com>
2022-12-19 20:33:13 +0300

    Fixed icon menu issue

--- jsfile.js
+++ jsfile.js
@@ -1,16 +1,17 @@
+let icon = document.querySelector(".icon i");
+let x = document.getElementById("myLinks");
+
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  var closeIcon = document.querySelector(".close-icon");
-  var barsIcon = document.querySelector(".icon");
-  if (x.classList.contains("open")) {
-    x.classList.remove("open");
-    x.style.display = "none";
-    closeIcon.style.display = "none";
-    barsIcon.style.display = "block";
-  } else {
-    x.classList.add("open");
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
     x.style.display = "flex";
-    closeIcon.style.display = "block";
-    barsIcon.style.display = "none";
+
+  } else {
+    
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+    x.style.display = "none";
+
   }
 }
