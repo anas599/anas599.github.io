fce2604e96cc64a0f8c62ee16f08136041e0efa1
AstroboyReloaded <astroboy.reloaded@gmail.com>
2022-12-20 14:50:01 -0600

    Add-EL

--- Assets/popUpObject.js
+++ Assets/popUpObject.js
@@ -56,26 +56,49 @@ let projectsInfo = [
 ];
 
 let projectsCont = document.getElementById('work-container');
-const popUp = document.querySelector('popUp');
 
 projectsCont.innerHTML = projectsInfo
   .map(
-    (project) => `
+    (project, i) => `
 <article>
   <img src='${project.featuredImage}'>
   <h1>${project.name}</h1>
   <ul class='lang'>${project.technologies
     .map((tech) => `<li class='l1'>${tech}</li>`)
     .join('')}</ul>
-  <button id='see-project-btn' onclick='popFunct()'>See Project</button>
+  <button id='see-project-btn' class='project${i}' onclick='popUpFun()'>See Project</button>
 </article>`
   )
   .join('');
 
 let seeProjectBtn = document.getElementById('see-project-btn');
-function popFunct() {
-  if (popUp.style.display === 'none') {
-    popUp.style.display === 'flex';
-  }
+const popUp = document.querySelector('.popUp');
+
+function popUpFun() {
+  popUp.style.display = 'flex';
 }
-seeProjectBtn.addEventListener('click', popFunct);
+
+let closePopUp = document.querySelector('.close-popUp');
+
+function close() {
+  popUp.style.display = 'none';
+}
+
+closePopUp.addEventListener('click', close);
+popUp = document.getElementById('popup');
+popUp.innerHTML = `
+  <img src="${details.featuredImage}" alt="${details.description}">
+  <h3 class="multi">${details.name}</h3>
+  <p>${details.description}</p>
+  <ul class="lang">${details.languages
+    .map((language) => `<li class="l1">${language}</li>`)
+    .join('')}</ul>
+`;
+
+// popUp.innerHTML = `<div>
+// <img src='${projectsInfofeaturedImage}'>
+//   <h1>${projectsInfo[0].name}</h1>
+//   <ul class='lang'>${projectsInfo.project.technologies
+//     .map((tech) => `<li class='l1'>${tech}</li>`)
+//     .join('')}</ul>
+// </div>`;
