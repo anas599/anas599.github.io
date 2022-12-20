56574c80f80cd8d8fe57870165585814547279db
AstroboyReloaded <astroboy.reloaded@gmail.com>
2022-12-20 13:41:40 -0600

    AddingEL

--- Assets/popUpObject.js
+++ Assets/popUpObject.js
@@ -1,58 +1,81 @@
 let projectsInfo = [
-  Project1: {
+  {
     name: 'Multi-Post Stories Gain+Glory',
-    description: './Assets/desc1.txt',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: './Assets/Snapshoot.png',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
     liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/'
+    sourceLink: 'https://anas599.github.io/',
   },
-  Project2: {
+  {
     name: 'Multi-Post Stories Gain+Glory',
-    description: './Assets/desc1.txt',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: './Assets/Snapshoot.png',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
     liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/'
+    sourceLink: 'https://anas599.github.io/',
   },
-  Project3: {
+  {
     name: 'Multi-Post Stories Gain+Glory',
-    description: './Assets/desc1.txt',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: './Assets/Snapshoot.png',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
     liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/'
+    sourceLink: 'https://anas599.github.io/',
   },
-  Project4: {
+  {
     name: 'Multi-Post Stories Gain+Glory',
-    description: './Assets/desc1.txt',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: './Assets/Snapshoot.png',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
     liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/'
+    sourceLink: 'https://anas599.github.io/',
   },
-  Project5: {
+  {
     name: 'Multi-Post Stories Gain+Glory',
-    description: './Assets/desc1.txt',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: './Assets/Snapshoot.png',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
     liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/'
+    sourceLink: 'https://anas599.github.io/',
   },
-  Project6: {
+  {
     name: 'Multi-Post Stories Gain+Glory',
-    description: './Assets/desc1.txt',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: './Assets/Snapshoot.png',
     technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
     liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/'
+    sourceLink: 'https://anas599.github.io/',
+  },
+];
+
+let projectsCont = document.getElementById('work-container');
+const popUp = document.querySelector('popUp');
+
+projectsCont.innerHTML = projectsInfo
+  .map(
+    (project) => `
+<article>
+  <img src='${project.featuredImage}'>
+  <h1>${project.name}</h1>
+  <ul class='lang'>${project.technologies
+    .map((tech) => `<li class='l1'>${tech}</li>`)
+    .join('')}</ul>
+  <button id='see-project-btn' onclick='popFunct()'>See Project</button>
+</article>`
+  )
+  .join('');
+
+let seeProjectBtn = document.getElementById('see-project-btn');
+function popFunct() {
+  if (popUp.style.display === 'none') {
+    popUp.style.display === 'flex';
   }
-]
-const workContainer = document.getElementById('work-container');
-workContainer.innerHTML = projectsInfo.forEach((project, i) => {return `
-  <article>
-  <img src="${project.featuredImage}" alt="${projectsInfo[project].description}">
-  <h3>${projectsInfo[project].name}</h3>
-  <p>${projectsInfo[project].description}</p>
-  <ul>${projectsInfo[project].technologies}</ul>
-  </article>`});
+}
+seeProjectBtn.addEventListener('click', popFunct);
