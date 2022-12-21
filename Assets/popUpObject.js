a8e778a50934f9a392e7e1abda338fb9415204d1
anas599 <anas599@yahoo.com>
2022-12-21 22:27:29 +0300

    Added Close popup function

--- Assets/popUpObject.js
+++ Assets/popUpObject.js
@@ -1,104 +1,120 @@
-let projectsInfo = [
+const projectsInfo = [
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './Assets/Snapshoot.png',
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
-    liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/',
+    featuredImage: "./Assets/Snapshoot.png",
+    featuredImageDesktop: "./Assets/Snapshoot Portfolio desktop.png",
+    technologies: ["Ruby on rails", "CSS", "JavaScript", "HTML"],
+    liveLink: "https://github.com/anas599/anas599.github.io",
+    sourceLink: "https://anas599.github.io/",
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './Assets/Snapshoot.png',
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
-    liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/',
+    featuredImage: "./Assets/Snapshoot.png",
+    featuredImageDesktop: "./Assets/Snapshoot Portfolio desktop.png",
+    technologies: ["Ruby on rails", "CSS", "JavaScript", "HTML"],
+    liveLink: "https://github.com/anas599/anas599.github.io",
+    sourceLink: "https://anas599.github.io/",
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './Assets/Snapshoot.png',
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
-    liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/',
+    featuredImage: "./Assets/Snapshoot.png",
+    featuredImageDesktop: "./Assets/Snapshoot Portfolio desktop.png",
+    technologies: ["Ruby on rails", "CSS", "JavaScript", "HTML"],
+    liveLink: "https://github.com/anas599/anas599.github.io",
+    sourceLink: "https://anas599.github.io/",
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './Assets/Snapshoot.png',
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
-    liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/',
+    featuredImage: "./Assets/Snapshoot.png",
+    featuredImageDesktop: "./Assets/Snapshoot Portfolio desktop.png",
+    technologies: ["Ruby on rails", "CSS", "JavaScript", "HTML"],
+    liveLink: "https://github.com/anas599/anas599.github.io",
+    sourceLink: "https://anas599.github.io/",
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './Assets/Snapshoot.png',
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
-    liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/',
+    featuredImage: "./Assets/Snapshoot.png",
+    featuredImageDesktop: "./Assets/Snapshoot Portfolio desktop.png",
+    technologies: ["Ruby on rails", "CSS", "JavaScript", "HTML"],
+    liveLink: "https://github.com/anas599/anas599.github.io",
+    sourceLink: "https://anas599.github.io/",
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
+    name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './Assets/Snapshoot.png',
-    technologies: ['Ruby on rails', 'CSS', 'JavaScript'],
-    liveLink: 'https://github.com/anas599/anas599.github.io',
-    sourceLink: 'https://anas599.github.io/',
+    featuredImage: "./Assets/Snapshoot.png",
+    featuredImageDesktop: "./Assets/Snapshoot Portfolio desktop.png",
+    technologies: ["Ruby on rails", "CSS", "JavaScript", "HTML"],
+    liveLink: "https://github.com/anas599/anas599.github.io",
+    sourceLink: "https://anas599.github.io/",
   },
 ];
 
-let projectsCont = document.getElementById('work-container');
+const projectsCont = document.getElementById('work-container');
 
 projectsCont.innerHTML = projectsInfo
   .map(
     (project, i) => `
-<article>
+<article class="allw">
   <img src='${project.featuredImage}'>
-  <h1>${project.name}</h1>
+  <h3>${project.name}</h3>
   <ul class='lang'>${project.technologies
     .map((tech) => `<li class='l1'>${tech}</li>`)
-    .join('')}</ul>
-  <button id='see-project-btn' class='project${i}' onclick='popUpFun()'>See Project</button>
-</article>`
+    .join("")}</ul>
+  <button id='see-project-btn' class='see' onclick='popUpFun()'>See Project</button>
+</article>` //class='project${i}
   )
-  .join('');
+  .join("");
 
-let seeProjectBtn = document.getElementById('see-project-btn');
+const seeProjectBtn = document.getElementById('see-project-btn');
 const popUp = document.querySelector('.popUp');
+const blurHead = document.getElementById('mainHead');
 
 function popUpFun() {
-  popUp.style.display = 'flex';
+  if(popUp.style.display = "none"){
+  popUp.style.display = "flex";
+    // if(popUp.style.display = "flex"){
+    //   popUp.style.display = "none"
+    // }
+}
+  // popUp.style.just
+  blurHead.style.filter = "blur(20px) contrast(50%)";
+}
+function closePopUp1() {
+    if(popUp.style.display = "flex"){
+      popUp.style.display = "none"
+      blurHead.style.filter = "none";
+
+    }
 }
 
-let closePopUp = document.querySelector('.close-popUp');
 
-function close() {
-  popUp.style.display = 'none';
-}
 
 popUp.innerHTML = `
-<button class="close-popUp" onclick="close()">X</button>
-<img style='width: 20%' src='${projectsInfo[1].featuredImage}'>
-<h1>${projectsInfo[0].name}</h1>
-<ul>${projectsInfo[0].technologies
+<div id="imageCloseBtn">
+  <button class="close-popUp fa fa-times"   onclick='closePopUp1()'></button>
+  <img id="imagePopUp" src='${projectsInfo[1].featuredImageDesktop}'>
+  </div>
+<h3>${projectsInfo[0].name}</h3>
+<ul class='lang'>${projectsInfo[0].technologies
   .map((tech) => `<li class='l1'>${tech}</li>`)
-  .join('')}
+  .join("")}
 </ul>
-<p>${projectsInfo[0].description}`;
+</ul>
+<p style="width: 80vw">${projectsInfo[0].description}</p>
+<div id="buttonsLive">
+<button id='see-project-btnPop  ' class='see fa fa-arrow-circle-up' style='width: 150'>   See Source</button>
+<button id='see-project-btnPop' class='see fa fa-github'style='width: 150 onclick="window.location.href='https://github.com'">See project</button></div>
 
-closePopUp.addEventListener('click', close);
-// popUp.innerHTML = `<div>
-// <img src='${projectsInfofeaturedImage}'>
-//   <h1>${projectsInfo[0].name}</h1>
-//   <ul class='lang'>${projectsInfo.project.technologies
-//     .map((tech) => `<li class='l1'>${tech}</li>`)
-//     .join('')}</ul>
-// </div>`;
+`;
