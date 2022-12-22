83b7b9e9dc83902a4b6c4c0c4b90665b14e90830
AstroboyReloaded <astroboy.reloaded@gmail.com>
2022-12-22 15:32:31 -0600

    Retreive data finctionality

--- Assets/localStorage.js
+++ Assets/localStorage.js
@@ -1,19 +1,24 @@
-const nameLc = document.getElementById("name");
-const emailLc = document.getElementById("email");
-const msgLc = document.getElementById("message");
-const ev = document.getElementsByClassName("formInput");
+const nameLc = document.getElementById('name');
+const emailLc = document.getElementById('email');
+const msgLc = document.getElementById('message');
+const ev = document.getElementsByClassName('formInput');
 let retData = JSON.parse(localStorage.formData);
 
 for (let i = 0; i < ev.length; i++) {
-    ev[i].addEventListener("input", function localS() {
-        localStorage.setItem(
-            "formData",
-            JSON.stringify({
-                name: nameLc.value,
-                email: emailLc.value,
-                message: msgLc.value,
-            })
-            );
-        });
-    }
-    
+  ev[i].addEventListener('input', function localS() {
+    localStorage.setItem(
+      'formData',
+      JSON.stringify({
+        name: nameLc.value,
+        email: emailLc.value,
+        message: msgLc.value,
+      })
+    );
+  });
+}
+
+window.onload = () => {
+  nameLc.value = retData.name;
+  emailLc.value = retData.email;
+  msgLc.value = retData.message;
+};
