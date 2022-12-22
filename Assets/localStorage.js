1e46c01503c4dae0f0e3708f61143593e4556cc6
anas599 <anas599@yahoo.com>
2022-12-23 01:34:54 +0300

    fixed eslint error

--- Assets/localStorage.js
+++ Assets/localStorage.js
@@ -5,22 +5,21 @@ const ev = document.getElementsByClassName('formInput');
 
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < ev.length; i++) {
-    ev[i].addEventListener('input', () => {
-        localStorage.setItem(
-            'formData',
-            JSON.stringify({
-                name: nameLc.value,
-                email: emailLc.value,
-                message: msgLc.value,
-            }),
-            );
-        });
-    }
-    
-    const retData = JSON.parse(localStorage.formData);
+  ev[i].addEventListener('input', () => {
+    localStorage.setItem(
+      'formData',
+      JSON.stringify({
+        name: nameLc.value,
+        email: emailLc.value,
+        message: msgLc.value,
+      }),
+    );
+  });
+}
+
+const retData = JSON.parse(localStorage.formData);
 window.onload = () => {
   nameLc.value = retData.name;
   emailLc.value = retData.email;
   msgLc.value = retData.message;
 };
-console.log('test1');
