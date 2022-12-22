1a92bb750efe4ef9c60c7e93d630440d78133e64
anas599 <anas599@yahoo.com>
2022-12-23 01:32:57 +0300

    changed JSON line

--- Assets/localStorage.js
+++ Assets/localStorage.js
@@ -2,24 +2,25 @@ const nameLc = document.getElementById('name');
 const emailLc = document.getElementById('email');
 const msgLc = document.getElementById('message');
 const ev = document.getElementsByClassName('formInput');
-const retData = JSON.parse(localStorage.formData);
 
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < ev.length; i++) {
-  ev[i].addEventListener('input', () => {
-    localStorage.setItem(
-      'formData',
-      JSON.stringify({
-        name: nameLc.value,
-        email: emailLc.value,
-        message: msgLc.value,
-      }),
-    );
-  });
-}
-
+    ev[i].addEventListener('input', () => {
+        localStorage.setItem(
+            'formData',
+            JSON.stringify({
+                name: nameLc.value,
+                email: emailLc.value,
+                message: msgLc.value,
+            }),
+            );
+        });
+    }
+    
+    const retData = JSON.parse(localStorage.formData);
 window.onload = () => {
   nameLc.value = retData.name;
   emailLc.value = retData.email;
   msgLc.value = retData.message;
 };
+console.log('test1');
