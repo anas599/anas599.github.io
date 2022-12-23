15efc92a0d33eb33000c39f1861d075ce3b3fcff
anas599 <anas599@yahoo.com>
2022-12-23 17:11:47 +0300

    Added images to popUp windows

--- Assets/localStorage.js
+++ Assets/localStorage.js
@@ -1,3 +1,14 @@
-const myForm = document.getElementById("contact_form");
-let myInput=document.querySelector('input')
-let myFormData={}
+const form = document.querySelector('form');
+// eslint-disable-next-line no-use-before-define
+form.addEventListener('submit', validateForm);
+function validateForm(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email').value;
+  if (email.toLowerCase() !== email) {
+    const errorMsg = document.querySelector('#errorMsg');
+    errorMsg.textContent = 'Email must be in lower case!';
+    return;
+  }
+  form.submit();
+}
