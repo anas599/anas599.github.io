b8a74ccb4cd59a27026f85b4d821e7b030c88e75
anas599 <anas599@yahoo.com>
2023-07-10 17:33:37 +0300

    testing load more

--- assets/js/scrollTransitions.js
+++ assets/js/scrollTransitions.js
@@ -1,11 +1,11 @@
-const observer = new IntersectionObserver(entries => {
-	entries.forEach(x => {
-		if (x.isIntersecting) {
-			x.target.classList.add('showScroll');
-		} else {
-			x.target.classList.remove('showScroll');
-		}
-	});
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((x) => {
+    if (x.isIntersecting) {
+      x.target.classList.add('showScroll');
+    } else {
+      x.target.classList.remove('showScroll');
+    }
+  });
 });
 const hiddenElements = document.querySelectorAll('.scroll');
-hiddenElements.forEach(el => observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el));
