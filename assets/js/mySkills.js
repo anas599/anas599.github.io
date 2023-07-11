e89d19671b2fb81e36611e81a1ca73da1a43f061
anas599 <anas599@yahoo.com>
2023-07-11 13:27:03 +0300

    fix eslint errors

--- assets/js/mySkills.js
+++ assets/js/mySkills.js
@@ -1,53 +1,53 @@
 const mySkills = [
-  { name: 'Languages', skills: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'SQL'] },
-  {
-    name: 'Frameworks',
-    skills: [
-      'Reactjs',
-      'Nextjs',
-      'Ruby on Rails',
-      'CapyBara',
-      'Bootstrap',
-      'Tailwind CSS',
-      'Sass',
-    ],
-  },
-  {
-    name: 'Tools',
-    skills: [
-      'Git',
-      'GitHub',
-      'VSCode',
-      'AWS Cloud computing',
-      'Postman',
-      'Heroku',
-      'Netlify',
-      'RSpec',
-      'TDD',
-      'Linux',
-      'Agile',
-    ],
-  },
+	{name: 'Languages', skills: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'SQL']},
+	{
+		name: 'Frameworks',
+		skills: [
+			'Reactjs',
+			'Nextjs',
+			'Ruby on Rails',
+			'CapyBara',
+			'Bootstrap',
+			'Tailwind CSS',
+			'Sass',
+		],
+	},
+	{
+		name: 'Tools',
+		skills: [
+			'Git',
+			'GitHub',
+			'VSCode',
+			'AWS Cloud computing',
+			'Postman',
+			'Heroku',
+			'Netlify',
+			'RSpec',
+			'TDD',
+			'Linux',
+			'Agile',
+		],
+	},
 ];
 const skills = document.querySelector('#skillsSection');
 
-mySkills.forEach((skill) => {
-  const skillDiv = document.createElement('div');
-  skillDiv.classList.add('card');
-  skillDiv.classList.add('glass');
-  skillDiv.classList.add('d-flex');
-  skillDiv.classList.add('h-w-15rem');
-  skillDiv.classList.add('align-items-center');
-  skillDiv.innerHTML = `
+mySkills.forEach(skill => {
+	const skillDiv = document.createElement('div');
+	skillDiv.classList.add('card');
+	skillDiv.classList.add('glass');
+	skillDiv.classList.add('d-flex');
+	skillDiv.classList.add('h-w-15rem');
+	skillDiv.classList.add('align-items-center');
+	skillDiv.innerHTML = `
             <h3 class="major">${skill.name}</h3>
             <ul class="skills-pill-container">
                 ${skill.skills
-                  .map(
-                    (skill) =>
-                      `<li class="badge badge-primary rounded-pill py-2 px-3 mb-2">${skill}</li>`,
-                  )
-                  .join('')}
+		.map(
+			skill =>
+				`<li class="badge badge-primary rounded-pill py-2 px-3 mb-2">${skill}</li>`,
+		)
+		.join('')}
             </ul>
         `;
-  skills.appendChild(skillDiv);
+	skills.appendChild(skillDiv);
 });
--- assets/js/new-top-nav.js
+++ assets/js/new-top-nav.js
@@ -1,37 +1,37 @@
 function newTopNav() {
-  const neonBtns = document.querySelectorAll(".neon-button__6");
-  const topNav = document.querySelector(".top-nav");
-  const navBtn = document.getElementById("navButton");
-  const icon = navBtn.querySelector("i");
-  const line1 = document.querySelector(".line-start");
-  const line2 = document.querySelector(".line-end");
-  function toggleNav() {
-    topNav.classList.toggle("top-nav--open");
-    neonBtns.forEach((neonBtn) => {
-      neonBtn.classList.toggle("neon-button__6--open");
-    });
+	const neonBtns = document.querySelectorAll('.neon-button__6');
+	const topNav = document.querySelector('.top-nav');
+	const navBtn = document.getElementById('navButton');
+	const icon = navBtn.querySelector('i');
+	const line1 = document.querySelector('.line-start');
+	const line2 = document.querySelector('.line-end');
+	function toggleNav() {
+		topNav.classList.toggle('top-nav--open');
+		neonBtns.forEach(neonBtn => {
+			neonBtn.classList.toggle('neon-button__6--open');
+		});
 
-    // Toggle the icon based on the open/closed state
-    if (topNav.classList.contains("top-nav--open")) {
-      icon.classList.remove("fa-bars");
-      icon.classList.add("fa-times");
-      line1.style.display = "flex";
-      line2.style.display = "flex";
-    } else {
-      icon.classList.remove("fa-times");
-      icon.classList.add("fa-bars");
-      line1.style.display = "none";
-      line2.style.display = "none";
-    }
-  }
+		// Toggle the icon based on the open/closed state
+		if (topNav.classList.contains('top-nav--open')) {
+			icon.classList.remove('fa-bars');
+			icon.classList.add('fa-times');
+			line1.style.display = 'flex';
+			line2.style.display = 'flex';
+		} else {
+			icon.classList.remove('fa-times');
+			icon.classList.add('fa-bars');
+			line1.style.display = 'none';
+			line2.style.display = 'none';
+		}
+	}
 
-  if (neonBtns) {
-    neonBtns.forEach((neonBtn) => {
-      neonBtn.addEventListener("click", toggleNav);
-    });
-  }
+	if (neonBtns) {
+		neonBtns.forEach(neonBtn => {
+			neonBtn.addEventListener('click', toggleNav);
+		});
+	}
 
-  navBtn.addEventListener("click", toggleNav);
+	navBtn.addEventListener('click', toggleNav);
 }
 
 newTopNav();
