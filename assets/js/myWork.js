9e86212bd65683266bea0d170f808edb14cca348
anas599 <anas599@yahoo.com>
2023-10-13 16:28:04 +0300

    update supplements register live url

--- assets/js/myWork.js
+++ assets/js/myWork.js
@@ -7,7 +7,7 @@ const projectsInfo = [
     featuredImage: './images/work/Supp-register.webp',
     featuredImageDesktop: './images/work/Supp-register.webp',
     technologies: ['Nextjs', 'Tailwind', 'PSQL', 'S3', 'Prisma', 'API'],
-    liveLink: 'https://supplements-register.netlify.app/',
+    liveLink: 'https://dev--supplements-register.netlify.app/',
     sourceLink: 'https://github.com/anas599/Supplement-register',
   },
   {
